b3bd344a501fd7517a618644ad355ffc739b1e51
agilsibawayh01-design <agilsibawayh01@gmail.com>
2026-09-10 14:25:00 +0700

    Create ArgoSearchSignatures.kt

---
+++ app/src/main/java/com/safetelegram/guard/domain/ArgoSearchSignatures.kt
@@ -0,0 +1,7 @@
+package com.safetelegram.guard.domain
+
+object ArgoSearchSignatures {
+    const val USERNAME = "@argo"
+    const val NORMALIZED_DISPLAY_NAME = "argo search"
+    const val DESCRIPTION_FRAGMENT = "telegram's search engine"
+}
